6e81a26b12e9d602e75341f1be704df2888b95a9
monowar <smhssain@gmail.com>
2017-08-27 00:22:24 -0500

    Initial commit

---
+++ utilities/src/main/java/org/md2k/utilities/ServicemCerebrum.java
@@ -0,0 +1,16 @@
+package org.md2k.utilities;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ServicemCerebrum extends Service {
+    public ServicemCerebrum() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
