db41d5e2cdf948686259cc0cbec28141b6bad46b
Xavier Tilley <hippietilley@gmail.com>
2018-02-26 10:20:01 -0600

    MC-765 Documented AppFromJson

--- system/src/main/java/org/md2k/mcerebrum/system/appinfo/AppFromJson.java
+++ system/src/main/java/org/md2k/mcerebrum/system/appinfo/AppFromJson.java
@@ -38,27 +38,36 @@ import rx.Observable;
 import rx.Subscriber;
 
 /**
- *
+ * Provides methods for checking for updates for apps installed from JSON.
  */
 public class AppFromJson {
     /**
+     * Gets the version information and creates an observer to check for updates.
+     *
      * @param context Android context.
      * @param downloadLink URL to download the application from.
-     * @return
+     * @return A <code>VersionInfo</code> observer.
      */
     public Observable<VersionInfo> getVersion(final Context context, final String downloadLink) {
         return Observable.create(new Observable.OnSubscribe<VersionInfo>() {
+            /**
+             * Creates a new <code>AppUpdaterUtils</code> object with a listener and starts that service.
+             * @param subscriber A new <code>VersionInfo</code> subscriber.
+             */
             @Override
             public void call(final Subscriber<? super VersionInfo> subscriber) {
                 try {
                     AppUpdaterUtils appUpdaterUtils = new AppUpdaterUtils(context)
                             .setUpdateFrom(UpdateFrom.JSON)
                             .setUpdateJSON(downloadLink)
-                            //.setUpdateFrom(UpdateFrom.AMAZON)
-                            //.setUpdateFrom(UpdateFrom.GITHUB)
-                            //.setGitHubUserAndRepo("javiersantos", "AppUpdater")
-                            //...
                             .withListener(new AppUpdaterUtils.UpdateListener() {
+                                /**
+                                 * Updates the <code>VersionInfo</code> fields, then passes the
+                                 * data to the subcriber and signals the completion.
+                                 *
+                                 * @param update The current update.
+                                 * @param isUpdateAvailable Whether a new update is available.
+                                 */
                                 @Override
                                 public void onSuccess(Update update, Boolean isUpdateAvailable) {
                                     VersionInfo versionInfo = new VersionInfo();
@@ -66,19 +75,24 @@ public class AppFromJson {
                                     versionInfo.versionCode = update.getLatestVersionCode();
                                     versionInfo.downloadURL = update.getUrlToDownload().toString();
                                     versionInfo.releaseInfo = update.getReleaseNotes();
-                                    subscriber.onNext(versionInfo);    // Pass on the data to subscriber
-                                    subscriber.onCompleted();     // Signal about the completion subscriber
+                                    subscriber.onNext(versionInfo);
+                                    subscriber.onCompleted();
                                 }
 
+                                /**
+                                 * Passes a null to the subscriber and then signals completion.
+                                 *
+                                 * @param error The app update error that occured.
+                                 */
                                 @Override
                                 public void onFailed(AppUpdaterError error) {
-                                    subscriber.onNext(null);    // Pass on the data to subscriber
-                                    subscriber.onCompleted();     // Signal about the completion subscriber
+                                    subscriber.onNext(null);
+                                    subscriber.onCompleted();
                                 }
                             });
                     appUpdaterUtils.start();
                 } catch (Exception e) {
-                    subscriber.onError(e);        // Signal about the error to subscriber
+                    subscriber.onError(e);
                 }
             }
         });
