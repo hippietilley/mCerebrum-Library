1f87e43a9c1f839dc098c80b13cbdf283e820b63
Xavier Tilley <hippietilley@gmail.com>
2018-02-28 14:47:29 -0600

    MC-765 Documented UserInfoCursor

--- core/src/main/java/org/md2k/mcerebrum/core/access/userinfo/UserInfoCursor.java
+++ core/src/main/java/org/md2k/mcerebrum/core/access/userinfo/UserInfoCursor.java
@@ -1,6 +1,32 @@
+/*
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.md2k.mcerebrum.core.access.userinfo;
 
-// @formatter:off
 import java.util.Date;
 
 import android.database.Cursor;
@@ -10,28 +36,36 @@ import android.support.annotation.Nullable;
 import org.md2k.mcerebrum.core.access.base.AbstractCursor;
 
 /**
- * Cursor wrapper for the {@code user_info} table.
+ * Cursor wrapper for the user info table.
  */
 @SuppressWarnings({"WeakerAccess", "unused", "UnnecessaryLocalVariable"})
 public class UserInfoCursor extends AbstractCursor implements UserInfoModel {
+    /**
+     * Constructor
+     *
+     * @param cursor
+     */
     public UserInfoCursor(Cursor cursor) {
         super(cursor);
     }
 
+
     /**
-     * Primary key.
+     * Returns the user identifier (Primary key).
+     * @return The user identifier (Primary key).
      */
     @Override
     public long getId() {
         Long res = getLongOrNull(UserInfoColumns._ID);
         if (res == null)
-            throw new NullPointerException("The value of '_id' in the database was null, which is not allowed according to the model definition");
+            throw new NullPointerException("The value of '_id' in the database was null, which is " +
+                    "not allowed according to the model definition");
         return res;
     }
 
     /**
-     * Get the {@code uid} value.
-     * Can be {@code null}.
+     * Returns the UID (Can be null).
+     * @return The UID (Can be null).
      */
     @Nullable
     @Override
@@ -41,8 +75,8 @@ public class UserInfoCursor extends AbstractCursor implements UserInfoModel {
     }
 
     /**
-     * Get the {@code type} value.
-     * Can be {@code null}.
+     * Returns the user type (Can be null).
+     * @return The user type (Can be null).
      */
     @Nullable
     @Override
@@ -52,8 +86,8 @@ public class UserInfoCursor extends AbstractCursor implements UserInfoModel {
     }
 
     /**
-     * Get the {@code username} value.
-     * Can be {@code null}.
+     * Returns the username (Can be null).
+     * @return The username (Can be null).
      */
     @Nullable
     @Override
