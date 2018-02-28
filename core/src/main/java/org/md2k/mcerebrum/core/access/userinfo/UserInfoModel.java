e90551362188db5ab4c337ace24bc2c44e1772af
Xavier Tilley <hippietilley@gmail.com>
2018-02-28 14:45:16 -0600

    MC-765 Documented UserInfoModel

--- core/src/main/java/org/md2k/mcerebrum/core/access/userinfo/UserInfoModel.java
+++ core/src/main/java/org/md2k/mcerebrum/core/access/userinfo/UserInfoModel.java
@@ -1,6 +1,32 @@
+/*
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.md2k.mcerebrum.core.access.userinfo;
 
-// @formatter:off
 import org.md2k.mcerebrum.core.access.base.BaseModel;
 
 import java.util.Date;
@@ -9,33 +35,35 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 /**
- * User information
+ * User information model interface
  */
 @SuppressWarnings({"WeakerAccess", "unused"})
 public interface UserInfoModel extends BaseModel {
 
     /**
-     * Primary key.
+     * Returns the user identifier (Primary key).
+     * @return The user identifier (Primary key).
      */
     long getId();
 
+
     /**
-     * Get the {@code uid} value.
-     * Can be {@code null}.
+     * Returns the UID (Can be null).
+     * @return The UID (Can be null).
      */
     @Nullable
     String getUid();
 
     /**
-     * Get the {@code type} value.
-     * Can be {@code null}.
+     * Returns the user type (Can be null).
+     * @return The user type (Can be null).
      */
     @Nullable
     String getType();
 
     /**
-     * Get the {@code username} value.
-     * Can be {@code null}.
+     * Returns the username (Can be null).
+     * @return The username (Can be null).
      */
     @Nullable
     String getUsername();
